384c965bdc1b366d5c67349320c73c86b1dfbc7f
Leonardo Antonio Dantas Ventocilla <lventocilla@oi.net.br>
2020-06-28 03:04:51 -0300

    Initial setup

---
+++ android/app/src/main/kotlin/com/example/squadapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.squadapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Squad',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hi Flutter'),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Text('Hi Flutter ...', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+        ),
+      ),
+    );;
+  }
+}
